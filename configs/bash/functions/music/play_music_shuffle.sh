d5b5961815c4d777c7f65acbf041c1fe952a499c
FZ <fz@noemail.org>
2026-02-16 18:12:39 +0100

    Modified function

--- configs/bash/functions/music/play_music_shuffle.sh
+++ configs/bash/functions/music/play_music_shuffle.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 play_music_shuffle() {
-	alias mm="mpv --input-ipc-server=/tmp/mpvsocket --shuffle \
-~/Music/"
+	music_dir=$1
+	if [ -z "$music_dir" ]; then
+		echo "Usage: play_music_shuffle <music_directory>"
+		return 1
+	fi
+	mpv --input-ipc-server=/tmp/mpvsocket --shuffle "$music_dir"
 }
+
+alias pms='play_music_shuffle'
